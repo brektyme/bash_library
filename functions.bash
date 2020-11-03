4bec4ea124a6281238068da15ecded4b37a26fa7
Joshua D Doll <joshua.doll@thebouqs.com>
2020-11-03 12:41:59 -0800

    initial commit
    * moved variables to variables.bash
    * fixed typo in pipefail option

--- functions.bash
+++ functions.bash
@@ -1,14 +1,7 @@
 #!/usr/bin/env bash
-set -euo pipefile
+set -euo pipefail
 IFS=$'\n\t'
 
-RED='\033[0;31m'
-NC='\033[0m'
-GREEN='\033[0;32m'
-
-export RED
-export NC
-export GREEN
 
 
 function warn() {
---
+++ variables.bash
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\t\n'
+
+
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+
+export RED
+export NC
+export GREEN
