5a24c0f355f0e5153d56996a19da60cd605f2ab3
Joshua D Doll <joshua.doll@thebouqs.com>
2021-01-28 12:27:27 -0800

    Add New Fucntion
    * added new function for returning absolute path of a file/dir/symlink

--- functions.bash
+++ functions.bash
@@ -5,6 +5,9 @@ IFS=$'\n\t'
 # shellcheck disable=1090
 source <(curl -s https://raw.githubusercontent.com/brektyme/bash_library/main/variables.bash)
 
+# source ./variables.bash
+
+
 
 function warn() {
     echo "$0: ${RED}$*${NC}">&2
@@ -16,3 +19,33 @@ function die() {
     warn "$@"
     exit "$rc"
 }
+
+function abs_path() {
+    [ -z "$1" ] && die 1 "${FUNCNAME[0]}: no arg passed!"
+    local __rel_path=${1:-''}
+    # [ -n "$REALPATH_CMD" ] && echo "$($REALPATH_CMD -z $__rel_path)"; return 0
+    # [ -n "$READLINK_CMD" ] && echo "$($READLINK_CMD -fz $__rel_path)"; return 0
+    
+    if [ -d "$__rel_path" ]; then
+        cd "$__rel_path"
+        pwd
+        return 0
+    fi
+    if [ -f "$__rel_path" ]; then
+        case "$__rel_path" in
+            /*)
+                echo "$__rel_path"
+                return 0
+                ;;
+            */*)
+                echo "$(cd "${__rel_path%/*}"; pwd)/${__rel_path##*/}"
+                return 0
+                ;;
+            *)
+                echo "$(pwd)/$__rel_path"
+                return 0
+                ;;
+        esac
+    fi
+}
+
--- variables.bash
+++ variables.bash
@@ -7,6 +7,12 @@ RED=$'\e[0;31m'
 NC=$'\e[0m'
 GREEN=$'\e[0;32m'
 
+
+REALPATH_CMD=$(command -v realpath || printf '')
+READLINK_CMD=$(command -v readlink || printf '')
+
 export RED
 export NC
 export GREEN
+export REALPATH_CMD
+export READLINK_CMD
