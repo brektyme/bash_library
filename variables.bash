9e1ece4fe0c4bf72aaa4acb92a153a43a5e376d2
Joshua D Doll <joshua.doll@thebouqs.com>
2021-01-28 17:08:52 -0800

    Add more color escape squences
    * added BOLD_GREEN, BOLD_RED, BOLD, and UL (Underline)

--- variables.bash
+++ variables.bash
@@ -6,6 +6,10 @@ IFS=$'\t\n'
 RED=$'\e[0;31m'
 NC=$'\e[0m'
 GREEN=$'\e[0;32m'
+BOLD_GREEN=$'\e[1;32m'
+BOLD_RED=$'\e[1;31m'
+BOLD=$'\e[1m'
+UL=$'\e[3m'
 
 
 REALPATH_CMD=$(command -v realpath || printf '')
@@ -14,5 +18,9 @@ READLINK_CMD=$(command -v readlink || printf '')
 export RED
 export NC
 export GREEN
+export BOLD
+export BOLD_RED
+export BOLD_GREEN
+export UL
 export REALPATH_CMD
 export READLINK_CMD
